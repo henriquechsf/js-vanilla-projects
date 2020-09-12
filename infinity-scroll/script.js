ffcc8cb05eaf110e19420ebdd82fa9a4ad9c2e97
Carlos Henrique S. Ferreira <ch.ferreir@hotmail.com>
2020-09-12 09:45:42 -0300

    refatorando setAttributes, com o conceito DRY

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -9,17 +9,32 @@ const count = 10;
 const apiKey = 'cqrJPEWRqor0Y8nW88fmcu91DM6LiCLZf3MPR2VhLaM';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Helper Function to set attributes on DOM elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 // Create elements for links & photos, add to DOM
 function displayPhotos() {
     // Run function for each object in PhotosArray
     photosArray.forEach(photo => {
         // Create <a> to link to Unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        });
 
         // Create image for photo
         const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
+
         img.setAttribute('src', photo.urls.regular);
         img.setAttribute('alt', photo.alt_description);
         img.setAttribute('title', photo.alt_description);
